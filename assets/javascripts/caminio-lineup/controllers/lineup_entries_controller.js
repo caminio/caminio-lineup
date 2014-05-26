31f4c253fb672df5733337e4d5d76cec62c05a10
thorsten zerha <thorsten.zerha@tastenwerk.com>
2014-05-26 14:38:24 +0200

    remove label

--- assets/javascripts/caminio-lineup/controllers/lineup_entries_controller.js
+++ assets/javascripts/caminio-lineup/controllers/lineup_entries_controller.js
@@ -95,7 +95,23 @@
   App.LabelItemController = Em.ObjectController.extend({
     isActive: function(){
       return this.get('parentController.searchQ.labels').findBy('id', this.get('content.id') );
-    }.property('parentController.searchQ.labels.@each')
+    }.property('parentController.searchQ.labels.@each'),
+    actions: {
+      'removeLabel': function( label ){
+        bootbox.confirm( Em.I18n.t('label.really_delete', {name: label.get('name')}), function( result ){
+          if( result ){
+            label.deleteRecord();
+            label.save()
+              .then(function(){
+                notify('info', Em.I18n.t('label.deleted', {name: label.get('name')}));
+              })
+              .catch( function(err){
+                notify('error', err);
+              });
+          }
+        });
+      }
+    }
   });
 
 })( App );
