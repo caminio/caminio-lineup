8f4921749f0f52c290165e1a9e0bc012c8c37aca
David Reinisch <david.reinisch@tastenwerk.com>
2014-05-27 17:08:26 +0200

    added index compiler

--- api/controllers/lineup_controller.js
+++ api/controllers/lineup_controller.js
@@ -1,6 +1,9 @@
 /**
  * @class LineupController
  */
+
+'use strict';
+
 module.exports = function LineupController( caminio, policies, middleware ){
 
   return {
@@ -16,6 +19,20 @@ module.exports = function LineupController( caminio, policies, middleware ){
       function( req, res ){
         res.caminio.render();
       }
+    ],
+
+    'compileAll':[
+      function( req, res ){
+        var SiteGen = require('caminio-rocksol/generator')( caminio );
+
+        var gen = new SiteGen( res.locals.currentDomain.getContentPath(), 'lineup' );
+
+        gen.compileLayout( 'index', { locals: res.locals }, function( err, content ){
+          console.log('WE GET: ', content );
+        });
+
+        res.send(200);
+      } 
     ]
 
   };
--- config/routes.js
+++ config/routes.js
@@ -4,5 +4,6 @@ module.exports.routes = {
   '/caminio/lineup': 'LineupController#index',
   'autorest /caminio/lineup_people': 'LineupPerson',
   'autorest /caminio/lineup_orgs': 'LineupOrg',
-  'autorest /caminio/lineup_entries': 'LineupEntry'
+  'autorest /caminio/lineup_entries': 'LineupEntry',
+  '/caminio/lineup_compile': 'LineupController#compileAll'
 };
